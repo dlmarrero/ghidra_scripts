278ed599ecf616303ff45b2181cbf0090d220c9c
Your Name <you@example.com>
2020-05-11 12:20:36 -0400

    Initial commit

---
+++ AutoRebase.py
@@ -0,0 +1,39 @@
+#Automatically rebase a relocatable ELF to a sane default
+#@author Davy L. Marrero
+#@category Helpers
+#@keybinding ctrl shift B
+#@menupath Tools.Helpers.Auto-Rebase
+
+
+def main():
+    # Bail if not an elf
+    if 'ELF' not in currentProgram.getExecutableFormat():
+        print('Binary not an elf. Will not rebase')
+        return
+
+    # Bail if the elf is not relocatable
+    if not currentProgram.relocationTable.isRelocatable():
+        print('Binary not relocatable. Will not rebase')
+        return
+
+    # Defaults are the same as gdb
+    DEFAULT_X86 = "0x400000"
+    DEFAULT_X64 = "0x555555554000"
+
+    # Select the base addr based on the arch
+    arch = str(currentProgram.getLanguageID())
+    if '64' in arch:
+        addr = DEFAULT_X64
+    elif '32' in arch:
+        addr = DEFAULT_X86
+    else:
+        raise Exception('Unknown architecture: ' + arch)
+
+    # Rebase the program
+    new_base = currentProgram.getAddressFactory().getAddress(addr)
+    currentProgram.setImageBase(new_base, True)
+
+    print("Rebased relocatable ELF to: " + str(new_base))
+
+
+main()
